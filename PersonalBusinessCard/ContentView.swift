10b6551915e4f5643809466edae8fb7fbe8f9829
Farhana Mustafa <farhana_mustafa@intuit.com>
2021-07-02 10:54:54 -0400

    Initial Commit

---
+++ PersonalBusinessCard/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PersonalBusinessCard
+//
+//  Created by fmustafa on 7/2/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ PersonalBusinessCard/PersonalBusinessCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  PersonalBusinessCardApp.swift
+//  PersonalBusinessCard
+//
+//  Created by fmustafa on 7/2/21.
+//
+
+import SwiftUI
+
+@main
+struct PersonalBusinessCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
